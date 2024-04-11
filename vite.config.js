bb003c82f825f08f480fcdab21d609eb2f3d17fa
Eth3ri0n <eth3rias@gmail.com>
2024-04-11 08:27:57 +0200

    Add vite.config.js with Vite configuration for building the project

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        form: resolve(__dirname, 'src/form/form.html'),
+      },
+    },
+    
+  },
+});
