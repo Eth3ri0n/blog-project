7d2a92cc76ec6d63d91a827b7c695ac6ce921e9c
Eth3ri0n <eth3rias@gmail.com>
2024-06-04 12:57:02 +0200

    refactor: Update modal.js to improve confirmation modal functionality

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -2,16 +2,13 @@ const body = document.querySelector('body');
 
 let Calc;
 let Modal;
+let Confirm;
+let Cancel;
 
 // Create calc.
 function Create_Calc() {
   Calc = document.createElement('div');
   Calc.classList.add('calc');
-  Calc.addEventListener('click', (event) => {
-    if (event.target.classList.contains('calc')) {
-      Calc.remove();
-    }
-  });
 }
 
 // Create modal.
@@ -21,15 +18,19 @@ function Create_Modal(question) {
   Modal.innerHTML = `<h5>${question}</h5>`;
 
   // Create buttons.
-  const CANCEL = document.createElement('button');
-  CANCEL.textContent = 'Cancel';
-  CANCEL.classList.add('btn', 'btn-danger');
-  const CONFIRM = document.createElement('button');
-  CONFIRM.textContent = 'Confirm';
-  CONFIRM.classList.add('btn', 'btn-primary');
+  Cancel = document.createElement('button');
+  Cancel.textContent = 'Cancel';
+  Cancel.classList.add('btn', 'btn-danger');
+  Confirm = document.createElement('button');
+  Confirm.textContent = 'Confirm';
+  Confirm.classList.add('btn', 'btn-primary');
+
+  Modal.addEventListener('click', (event) => {
+    event.stopPropagation();
+  });
 
   // Append buttons.
-  Modal.append(CANCEL, CONFIRM);
+  Modal.append(Cancel, Confirm);
 }
 
 export function Confirm_Open_Modal(question) {
@@ -38,4 +39,26 @@ export function Confirm_Open_Modal(question) {
 
   Calc.append(Modal);
   body.append(Calc);
+
+  return new Promise((resolve, reject) => {
+    // Resolve false if calc is clicked.
+    Calc.addEventListener('click', (event) => {
+      if (event.target.classList.contains('calc')) {
+        Calc.remove();
+        resolve(false);
+      }
+    });
+
+    // Resolve false if cancel button is clicked.
+    Cancel.addEventListener('click', () => {
+      Calc.remove();
+      resolve(false);
+    });
+
+    // Resolve true if confirm button is clicked.
+    Confirm.addEventListener('click', () => {
+      Calc.remove();
+      resolve(true);
+    });
+  });
 }
